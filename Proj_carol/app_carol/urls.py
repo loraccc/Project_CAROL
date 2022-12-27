9bb45d0232c869f36b238449cf2ebb8ee9ac7edf
loraccc <carolacharya1@gmail.com>
2022-12-27 11:32:29 +0545

    layouts ma create show and edit page created

--- Proj_carol/app_carol/urls.py
+++ Proj_carol/app_carol/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("variety/",views.variety_index,name="variety.index")
+    path("variety/",views.variety_index,name="variety.index"),
+    path("variety/show/",views.variety_show,name="variety.show"),
+    path("variety/edit/",views.variety_edit,name="variety.edit"),
+    path("variety/create/",views.variety_create,name="variety.create"),
+
 ]
--- Proj_carol/app_carol/views.py
+++ Proj_carol/app_carol/views.py
@@ -2,3 +2,12 @@ from django.shortcuts import render
 
 def variety_index(request):
     return render(request,"variety/index.html")
+
+def variety_show(request):
+    return render(request,"variety/show.html")
+
+def variety_edit(request):
+    return render(request,"variety/edit.html")
+    
+def variety_create(request):
+    return render(request,"variety/create.html")
